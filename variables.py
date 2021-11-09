6ed53ebb64b442030bde1ea2f6840cf4ce46b716
Emmanuel Okuchukwu <emmanza2@gmail.com>
2021-11-09 12:43:42 +0000

    first commit

---
+++ variables.py
@@ -0,0 +1,24 @@
+#Simple python string variable declaration
+x = 'Hello World!'
+print('Emmanuel says ' + x)
+
+#Simple python int variable declaration
+y = 50
+print(y)
+
+a = 40
+b = 65
+c = a + b
+print(c)
+
+#Simple python complex variable declaration
+h = 4j
+print(h)
+
+# How to find out what the datatypes are in variables
+a = 105
+b = 'God is Awesome'
+print(type(a))
+print(type(b))
+
+
